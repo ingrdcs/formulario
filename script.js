7a150c568fc460ecc8a09362cb1aebb4fddfe5af
Ingrid <65837228+ingrdcs@users.noreply.github.com>
2022-07-31 19:25:13 -0300

    update
    adicionando função verificarInputs()

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function formularioEnviado() {
-    alert("Formulário enviado com sucesso! Obrigada pelo seu tempo.")
+function verificarInputs() {
+    let nome = document.querySelector("#nome").value;
+    let email = document.querySelector("#email").value;
+    let idade = document.querySelector("#idade").value;
+    let Cargo = document.querySelector("#Cargo").value;
+    let opcoesRadio = document.querySelectorAll("input[type=radio]").value;
+    let areaDeInteresse = document.querySelector("#Area-de-interesse").value;
+    let opcoesCheckBox = document.querySelectorAll("input[type=checkbox]").value;
+    let textarea = document.querySelector("#outros").value;
+}
+
+if (nome == " " || email == " " || idade == " " || Cargo == " " || opcoesRadio == " " || areaDeInteresse == " " || opcoesCheckBox == " " || textarea == " ") {
+    alert("Preencha todos os campos!");
+} else {
+    formularioEnviado();
+
 }
